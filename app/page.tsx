b9f86ec2d34816422e0767f83c00496585b11dad
Bridger Tower <bridger@ziondesign.io>
2023-02-23 20:42:02 -0700

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,18 @@ import Experience from '@/components/Experience';
 import Footer from '@/components/Footer';
 import Library from '@/components/Library';
 import Dribbble from '@/components/Dribbble';
+import Testimonials from '@/components/Testimonials';
 
 export default function Home() {
 	return (
-		<main className='p-6 lg:p-0'>
+		<main className="p-6 lg:p-0">
 			<HeroCards />
 			<Intro />
 			<CaseStudies />
 			<Experience />
 			<Dribbble />
 			<Library />
+			<Testimonials />
 			<Footer />
 		</main>
 	);
